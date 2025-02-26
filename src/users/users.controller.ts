bd50df2e85fb74c19d0ca56bf1847e73d38766b1
Myourkiu <pedrofigsesteves@gmail.com>
2025-02-25 22:34:57 -0300

    feat/create user

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post, Body } from '@nestjs/common';
+import { UsersService } from './users.service';
+import { User } from 'src/models/user';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+    constructor(private readonly userService: UsersService){}
+
+    @Post()
+    create(@Body() newUser: User) {
+        this.userService.create(newUser)
+    }
+}
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,23 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable } from "@nestjs/common";
+import { User } from "src/models/user";
+import { hashSync } from "bcrypt";
+import { v4 as uuid } from "uuid";
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  private users: User[] = [
+    {
+      id: "123",
+      username: "Teste",
+      password: "senha",
+    },
+  ];
+
+  create(newUser: User) {
+    newUser.id = uuid();
+    newUser.password = hashSync(newUser.password, 10);
+
+    this.users.push(newUser);
+    console.log(this.users)
+  }
+}
