baf83ebb76f0c95ffbc30158a412e2345c249b2f
Pedro Figueiredo <pedrofigsesteves@gmail.com>
2025-06-29 16:13:33 -0300

    feat: add validation on DTOs

--- src/models/task.ts
+++ src/models/task.ts
@@ -1,12 +1,32 @@
+import { IsDateString, IsEnum, IsOptional, IsString, IsUUID, MaxLength, MinLength } from "class-validator";
+
+export enum TaskStatus {
+  TO_DO = 'TO_DO',
+  IN_PROGRESS = 'IN_PROGRESS',
+  DONE = 'DONE',
+}
+
 export class Task {
+  @IsUUID()
+  @IsOptional()
   id: string;
+  @IsString()
+  @MinLength(3)
+  @MaxLength(256)
   title: string;
+  @IsString()
+  @MinLength(3)
+  @MaxLength(512)
   description: string;
+
+  @IsEnum(TaskStatus)
+  @IsOptional()
   status: string;
+  @IsDateString()
   expirationDate: Date;
 }
 
-export interface IFindAllTasksParams{
-    title: string,
-    status: string
+export interface IFindAllTasksParams {
+  title: string;
+  status: string;
 }
--- src/task/task.service.ts
+++ src/task/task.service.ts
@@ -1,58 +1,71 @@
-import { HttpException, HttpStatus, Injectable, NotFoundException } from '@nestjs/common';
-import { IFindAllTasksParams, Task } from 'src/models/task';
+import {
+  HttpException,
+  HttpStatus,
+  Injectable,
+  NotFoundException,
+} from "@nestjs/common";
+import { IFindAllTasksParams, Task, TaskStatus } from "src/models/task";
+import { v4 as uuid } from "uuid";
 
 @Injectable()
 export class TaskService {
-    private tasks:Task[] = []
-    create(task:Task){
-        this.tasks.push(task)
+  private tasks: Task[] = [];
+  create(task: Task) {
+    task.id = uuid();
+    task.status = TaskStatus.TO_DO;
+    this.tasks.push(task);
+  }
+
+  findById(id: string): Task {
+    const taskFound = this.tasks.find((t) => t.id == id);
+
+    if (taskFound == undefined)
+      throw new NotFoundException(`Task with id ${id} not found`);
+
+    return taskFound;
+  }
+
+  findAll(params: IFindAllTasksParams): Task[] {
+    return this.tasks.filter((t) => {
+      let match = true;
+
+      if (params.title != undefined && !t.title.includes(params.title)) {
+        match = false;
+      }
+
+      if (params.status != undefined && !t.status.includes(params.status)) {
+        match = false;
+      }
+
+      return match;
+    });
+  }
+
+  update(task: Task) {
+    const taskIndex = this.tasks.findIndex((t) => t.id === task.id);
+
+    if (taskIndex >= 0) {
+      this.tasks[taskIndex] = task;
+      return;
     }
 
-    findById(id: string) : Task {
-        const taskFound = this.tasks.find(t => t.id == id);
+    throw new HttpException(
+      `Task with id ${task.id} not found`,
+      HttpStatus.BAD_REQUEST
+    );
+  }
 
-        if(taskFound == undefined)
-            throw new NotFoundException(`Task with id ${id} not found`)
+  remove(id: string) {
+    const taskIndex = this.tasks.findIndex((t) => t.id === id);
 
-        return taskFound
+    if (taskIndex >= 0) {
+      this.tasks.splice(taskIndex, 1);
+      return;
     }
 
-    findAll(params: IFindAllTasksParams) : Task[]{
-        return this.tasks.filter(t => {
-            let match = true;
-
-            if(params.title != undefined && !t.title.includes(params.title)){
-                match = false;
-            }
-
-            if(params.status != undefined && !t.status.includes(params.status)){
-                match = false;
-            }
-            
-            return match;
-        })
-    }
-
-    update(task: Task){
-        const taskIndex = this.tasks.findIndex(t => t.id === task.id)
-
-        if(taskIndex >= 0){
-            this.tasks[taskIndex] = task
-            return;
-        }
-
-        throw new HttpException(`Task with id ${task.id} not found`, HttpStatus.BAD_REQUEST)
-    }
-
-    remove(id: string){
-        const taskIndex = this.tasks.findIndex(t => t.id === id)
-
-        if(taskIndex >= 0){
-            this.tasks.splice(taskIndex, 1)
-            return;
-        }
-
-        throw new HttpException(`Task with id ${id} not found`, HttpStatus.BAD_REQUEST)
-    }
-
+    throw new HttpException(
+      `Task with id ${id} not found`,
+      HttpStatus.BAD_REQUEST
+    );
+  }
 }
