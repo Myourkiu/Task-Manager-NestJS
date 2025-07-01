1765fbed26e178c4cb77125096400ac7282c92a9
Pedro Figueiredo <pedrofigsesteves@gmail.com>
2025-07-01 12:05:42 -0300

    fix: user entity class name

--- src/db/entities/user.entity.ts
+++ src/db/entities/user.entity.ts
@@ -1,7 +1,7 @@
 import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
 
 @Entity({name: 'user'})
-export class TaskEntity{
+export class UserEntity{
     @PrimaryGeneratedColumn('uuid')
     id:string
 
